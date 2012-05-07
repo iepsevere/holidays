7b01f019301e14872bc24a5dd8b792bb9c54f56b
Geoffroy Gomet <geoffroy.gomet@telenet.be>
2012-05-07 20:30:41 +0200

    Capistrano integration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,20 @@
-set :application, "yunus.gomet.be"
-set :repository,  "set your repository location here"
+default_run_option[:pty] = true
 
-set :scm, :git
+set :application, "yunus.gomet.be"
+set :repository,  "git@github.com:iepsevere/holidays.git"
+
+set :user, "students"
+
+set :scm, "git"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, application                          # Your HTTP server, Apache/etc
 role :app, application                          # This may be the same as your `Web` server
 role :db,  application, :primary => true # This is where Rails migrations will run
+
+set :branch, "master"
+set :deploy_to, "/var/www/vhosts/gomet.be/yunus"
+set :deploy_via, :remote_cache
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -19,6 +27,7 @@ role :db,  application, :primary => true # This is where Rails migrations will r
    task :start do ; end
    task :stop do ; end
    task :restart, :roles => :app, :except => { :no_release => true } do
+     migrate
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
  end
