c4f03d4cafd02772088ea05daca5b089cc71b5c0
Geoffroy Gomet <geoffroy.gomet@telenet.be>
2012-05-07 19:57:29 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holidays::Application.config.secret_token = 'a5c0bae10fd853a56e5d5f14d62880ac4d0430f515b3892b3996b061db4710ca35656bd2e14f99d7e69b20cc0c0f4822f4a558ede136bf17e22ca58ec7f35fc8'
